4e4dd66a34d9b727d3729db47d622f55b5030221
Jesse Vogt <jesse.vogt@shopify.com>
2021-05-10 17:16:26 -0500

    properly detecting installed program

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
-ln -sf ~/dotfiles/commonrc ~/.zshrc
-
-if [ $SPIN ]; then
-  if ! command -v rg &> /dev/null; then
-    sudo apt-get install -y ripgrep
-  fi
+echo "\$SPIN=$SPIN"
+        
+if [ "$SPIN" ]; then
+        echo "detected spin environment"
+        if type rg > /dev/null 2>&1
+        then
+                echo "ripgrep already installed"
+        else
+                echo "installing rip-grep" 
+                sudo apt-get install -y ripgrep
+        fi
 fi
+
+ln -sf ~/dotfiles/commonrc ~/.zshrc
